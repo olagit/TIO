e89b30ee20cb000419c51e51720ed97aaf4acfce
olagit <olagit@users.noreply.github.com>
2016-04-18 16:44:32 +0200

    Added files via upload

---
+++ Zad5/WebApplication2/WebApplication2/Controllers/AuthorsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ObjectsManager.Models;
+using ObjectsManager.LiteDBAuthor;
+
+namespace WebApplication2.Controllers
+{
+    public class AuthorsController : ApiController
+    {
+        AuthorRepository ar = new AuthorRepository();
+
+        public IEnumerable<Author> Get()
+        {
+            return ar.GetAll();
+        }
+
+        public Author Get(int id)
+        {
+            return ar.Get(id);
+        }
+
+        public void Post([FromBody] Author a)
+        {
+            ar.Add(a);
+        }
+
+        public void Put(int id, [FromBody] Author a)
+        {
+            ar.Update(a);
+        }
+
+        public void Delete(int id)
+        {
+            ar.Delete(id);
+        }
+
+    }
+}
---
+++ Zad5/WebApplication2/WebApplication2/Controllers/BooksController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ObjectsManager.Models;
+using ObjectsManager.LiteDBBook;
+
+namespace WebApplication2.Controllers
+{
+    public class BooksController : ApiController
+    {
+        BookRepository br = new BookRepository();
+
+        public IEnumerable<Book> Get()
+        {
+            return br.GetAll();
+        }
+
+        public Book Get(int id)
+        {
+            return br.Get(id);
+        }
+
+        public IEnumerable<Book> Get(string search)
+        {
+            return br.GetAll().Where(x => x.BookTitle.Contains(search));
+        }
+
+        public void Post([FromBody] Book b)
+        {
+            br.Add(b);
+        }
+
+        public void Put(int id, [FromBody] Book b)
+        {
+            br.Update(b);
+        }
+
+        public void Delete(int id)
+        {
+            br.Delete(id);
+        }
+    }
+}
