db86bc1942bd8b9101b91a85af3529dc97357f31
olagit <olagit@users.noreply.github.com>
2016-04-25 16:11:39 +0200

    Added files via upload

---
+++ Zad6/WebApplication3/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(WebApplication3.Startup))]
+namespace WebApplication3
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+           
+        }
+    }
+}
